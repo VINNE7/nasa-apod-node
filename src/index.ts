a3d57822f4358317be4002ec8bd7caf5aa397696
VINNE7 <marcusv.ribas7@gmail.com>
2025-04-25 21:38:09 -0300

    feat: add errorHandler in index.ts globally

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 
 import express from "express";
 import apodRoutes from "./apod/apod.routes.js";
+import errorHandler from "./utils/middlewares/errorHandler.js";
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,8 @@ console.log("[ENV] NASA_API_URL =", process.env.NASA_API_URL);
 app.use(express.json());
 
 apodRoutes(app);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
