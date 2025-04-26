cff3afcda0bc1fccd6c83c5565d27b865dfe1771
VINNE7 <marcusv.ribas7@gmail.com>
2025-04-25 21:36:32 -0300

    chore: add typescript no usuned vars rule to ignore params or variables using the prefix _

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,15 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 );
